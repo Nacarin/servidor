a1ab4c9688e1738bdb009bdd24695fe8ee46bd00
Nacarin <146434913+Nacarin@users.noreply.github.com>
2023-10-26 20:33:40 +0200

    Add files via upload

---
+++ Muchas cosas de jesuitas/CRUD jesuitas servidorweb/clases/jesuita.php
@@ -0,0 +1,61 @@
+<?php
+    class Jesuita
+    {
+        private $db; // Representa la conexión a la base de datos
+
+        public function __construct($db)
+        {
+            $this->db = $db; // Constructor que recibe la conexión a la base de datos
+        }
+
+        // Método para agregar un nuevo Jesuita a la base de datos
+        public function agregarJesuita($idJesuita, $nombre, $firma)
+        {
+
+            // Construye la consulta SQL para insertar un nuevo Jesuita
+            $query = "INSERT INTO jesuita (idJesuita, nombre, firma) VALUES ($idJesuita, '$nombre', '$firma')";
+
+            // Ejecuta la consulta SQL y verifica si se realizó con éxito
+            if ($this->db->query($query)) {
+                return true; // Éxito al agregar el Jesuita
+            } else {
+                return false; // Error al agregar el Jesuita
+            }
+        }
+
+        // Método para modificar un Jesuita existente en la base de datos
+        public function modificarJesuita($idJesuita, $nombre, $firma)
+        {
+            // Convierte $idJesuita a un entero para garantizar que sea un número
+            $idJesuita = (int)$idJesuita;
+
+            // Construye la consulta SQL para actualizar el nombre y la firma del Jesuita
+            $query = "UPDATE jesuita SET nombre = '$nombre', firma = '$firma' WHERE idJesuita = $idJesuita";
+
+            // Ejecuta la consulta SQL y verifica si se realizó con éxito
+            if ($this->db->query($query)) {
+                return true; // Éxito al modificar el Jesuita
+            } else {
+                return false; // Error al modificar el Jesuita
+            }
+        }
+
+        // Método para borrar un Jesuita de la base de datos
+        public function borrarJesuita($idJesuita)
+        {
+            // Convierte $idJesuita a un entero para garantizar que sea un número
+            $idJesuita = (int)$idJesuita;
+
+            // Construye la consulta SQL para eliminar un Jesuita
+            $query = "DELETE FROM jesuita WHERE idJesuita = $idJesuita";
+
+            // Ejecuta la consulta SQL y verifica si se realizó con éxito
+            if ($this->db->query($query)) {
+                return true; // Éxito al borrar el Jesuita
+            } else {
+                return false; // Error al borrar el Jesuita
+            }
+        }
+        
+    }
+?>
---
+++ Muchas cosas de jesuitas/CRUD jesuitas servidorweb/vistas/listarlugar.php
@@ -0,0 +1,51 @@
+<?php
+    // Incluye los archivos de configuración de la base de datos y la clase Lugar
+    require_once('../configuracion/config.php');
+    require_once('../clases/lugar.php');
+    
+
+    // Inicializa una conexión a la base de datos
+    $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    if ($db->connect_error) {
+        die("Error de conexión a la base de datos: " . $db->connect_error);
+    }
+
+    // Crea una instancia de la clase Lugar y le pasa la conexión a la base de datos
+    $lugar = new Lugar($db);
+
+    // Llama al método para listar lugares
+    $lugaresLista = $lugar->listarLugares();
+
+    if (!empty($lugaresLista)) {
+        // Si la lista de lugares no está vacía, muestra los lugares en una tabla
+        echo '<h2>Lista de Lugares</h2>';
+        echo '<table>';
+        echo '<tr><th>IP</th><th>Nombre</th><th>Descripción</th></tr>';
+        foreach ($lugaresLista as $lugarDatos) {
+            // Itera a través de los lugares y muestra cada lugar en una fila de la tabla
+            echo '<tr>';
+            echo '<td>' . $lugarDatos['ip'] . '</td>';
+            echo '<td>' . $lugarDatos['lugar'] . '</td>';
+            echo '<td>' . $lugarDatos['descripcion'] . '</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+    } else {
+        // Si la lista de lugares está vacía, muestra un mensaje indicando que no hay lugares en la base de datos
+        echo 'No hay lugares en la base de datos.';
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Listado</title>
+        <link rel="stylesheet" type="text/css" href="../estilos/estilos.css">
+    </head>
+    <body>
+        <form></form>
+            <h2>Volver al Índice</h2>
+            <a href="../index.html">Inicio</a>
+        </form>
+    </body>
+</html>
